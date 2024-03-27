c08929be2aefa2bcdc2e547007b5dac7825be447
ваше имя <email@example.com>
2024-03-28 02:52:09 +0300

    task7

---
+++ JS/app.js
@@ -0,0 +1,12 @@
+//console.log("hello world")
+//console.log(1 + 2)//number
+//console.log("1" + "2") //string
+//console.log("hello" + " " + "world")
+//console.log(4 +"apple")
+//console.log(10 - "2" + "apple")
+
+// var myVar = 10;
+// var b = myVar + 9;
+// console.log(myVar);
+var myName = "Ilya";
+console.log("hello world")
